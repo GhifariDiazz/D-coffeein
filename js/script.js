f36a4eb124e28345f3a6b411441f62d94c587a05
GhifariDiazz <ghifaridiazz@gmail.com>
2025-02-20 10:34:32 +0700

    website kopi dcoffeein

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// tombol kelas aktif
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar nav
+
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
